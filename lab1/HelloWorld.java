f8b325c610a6922781d9c647a1abdf9c794b6f65
ji he <jihe@jis-iMac.home>
2021-06-15 20:40:23 -0400

    Add HelloWorld

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+ 	public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Ji Ji is the best!");
+        System.out.println("Duo duo is stupid!");
+    }
+
+}
